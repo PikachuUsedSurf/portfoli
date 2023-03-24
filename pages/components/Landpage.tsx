5ed7508969e0d4ddb1ed866bde8aba21fb02b2aa
PikachuUsedSurf <nanaosafokissi@gmail.com>
2023-03-24 14:22:37 +0300

    bio fix

--- pages/components/Landpage.tsx
+++ pages/components/Landpage.tsx
@@ -4,7 +4,7 @@ export default function Landpage() {
         <main className="flex-auto min-w-0 mt-6 md:mt-0 flex flex-col px-2 md:px-0 ">
             <section>
             <h1 className="text-3xl font-bold font-serif text-white">Nana yaw</h1>
-            <p className="my-5 max-w-[460px] text-neutral-800 dark:text-neutral-200">Hey, I'm Nana, <b>a software developer who can turn designs into code</b>. I'm a wizard casting spells with languages like Python and Javascript, but my true superpower is turning <b>designs</b> into code. </p>
+            <p className="my-5 max-w-[460px] text-neutral-800 dark:text-neutral-200">Hey, I&apos;m Nana, <b>a software developer who can turn designs into code</b>. I&apos;m a wizard casting spells with languages like Python and Javascript, but my true superpower is turning <b>designs</b> into code. </p>
             <div className="flex">
                 <img className=" rounded-full h-28" src="/images/profile-pic.png" alt="profile" />
                 <div className="dark:text-neutral-400 dark:hover:text-neutral-200 mt-8 md:mt-0 ml-0 md:ml-6 space-y-2 ">
@@ -13,7 +13,7 @@ export default function Landpage() {
                     <a href="" className="flex items-center ">My SideProjects</a>
                 </div>
             </div>
-            <p className=" text-neutral-800 dark:text-neutral-200 my-5 max-w-[600px]">When i'm not working on code, you can find me binge-watching the latest anime and manga series or sketching out a new character design. My passion for art and design informs my coding process, as i see the beauty in elegant solutions and clean code.</p>
+            <p className=" text-neutral-800 dark:text-neutral-200 my-5 max-w-[600px]">When i&apos;m not working on code, you can find me binge-watching the latest anime and manga series or sketching out a new character design. My passion for art and design informs my coding process, as i see the beauty in elegant solutions and clean code.</p>
             <ul className="Follow-links flex gap-4 font-sm text-neutral-500 dark:text-neutral-400 ">
                 <li>
                     <a className="flex items-center hover:text-neutral-700 dark:hover:text-neutral-200 transition-all" rel="noopener noreferrer" target="_blank" href="https://twitter.com/aurameow">
